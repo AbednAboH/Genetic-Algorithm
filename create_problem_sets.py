e86c39bea0f7a466d9b941656efa2aab4dfd140a
Abed <freazer122@gmail.com>
2022-03-04 16:09:57 +0200

    Generalized the algorithem
    1. created a class for general problems
    2. we use above class as a base class so that we can tweak it's parameters and functions depending on the problem so that whatever problem we use can be checked using the genetic algorithem without changing it

---
+++ create_problem_sets.py
@@ -0,0 +1,48 @@
+import random
+
+
+# basic class for all problem sets because fittness and the member of the population are problem specific
+# and we have to eliminate problem specifc parameters from the Genetic algorithem
+# might add mutate !
+class parameters:
+    def __init__(self):
+        self.object = None
+        self.fitness = 0
+
+
+    # creates a member of the population
+    def create_object(self,target_size):
+        return self.object
+
+    # function to calculate the fitness for this specific problem
+    def calculate_fittness(self,target, target_size):
+        return self.fitness
+
+    # for sorting purposes
+    def __lt__(self, other):
+        return self.fitness < other.fitness
+
+
+# class for first problem
+class DNA(parameters):
+    def __init__(self):
+        parameters.__init__(self)
+
+    def create_object(self,target_size):
+        self.object = ""
+        for j in range(target_size):
+            self.object += chr((random.randint(0, 90)) + 32)
+        return self.object
+
+    def calculate_fittness(self,target, target_size):
+        for j in range(target_size):
+            fit = ord(self.object[j]) - ord(target[j])
+            self.fitness += abs(fit)
+        return self.fitness
+    def mutate(self,target_size,member):
+        ipos = random.randint(0,target_size - 1)
+        delta = random.randint(0, 90) + 32
+        member.object.replace(member.object[ipos], chr((ord(member.object[ipos]) + delta) % 122))
+
+
+
