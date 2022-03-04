81e1b77131a4a66eb954a65375b0ddd1f07e0ef1
Abed <freazer122@gmail.com>
2022-03-04 21:10:17 +0200

    section 4 is done ! perfect

--- Genetic.py
+++ Genetic.py
@@ -39,8 +39,42 @@ class Ga_struct():
 """ to generalize the problem we created a class that given a population calculates the solution"""
 
 
+# given 2 samples(citizens) from the population calculate crossed sample
+class cross_types:
+
+    def __init__(self):
+        # maps functions to numbers so that we can choose which one to assign
+        self.select = {1: self.one_cross, 2: self.two_cross, 3: self.uniform_cross}
+
+    def one_cross(self, citizen1, citizen2):
+        target_size = len(citizen1)
+        spos = random.randint(0, target_size)
+        return citizen1[0:spos] + citizen2[spos:target_size], citizen2[0:spos] + citizen1[spos:target_size]
+
+    def two_cross(self, citizen1, citizen2):
+        target_size = len(citizen1)
+        spos = random.randint(0, target_size - 2)  # we need at least 3 portions
+        spos2 = random.randint(spos, target_size - 1)  # we need at least 2 portions
+        first = citizen1[0:spos] + citizen2[spos:spos2] + citizen1[spos2 :]
+        sec = citizen2[0:spos] + citizen1[spos:spos2] + citizen2[spos2:]
+        return first, sec
+
+    def uniform_cross(self, citizen1, citizen2):
+        target_size = len(citizen1)
+        object1 = []
+        object2 = []
+        for i in range(target_size):
+            if random.random() > 0.5:
+                object1.append(citizen2[i])
+                object2.append(citizen1[i])
+            else:
+                object1.append(citizen1[i])
+                object2.append(citizen2[i])
+        return object1, object2
+
+
 class genetic_algorithem:
-    def __init__(self, target, tar_size, pop_size, problem_spec):
+    def __init__(self, target, tar_size, pop_size, problem_spec, crosstype):
         self.population = list(range(pop_size))
         self.buffer = list(range(pop_size))
         self.target = target
@@ -49,7 +83,8 @@ class genetic_algorithem:
         self.pop_mean = 0
         self.iteration = 0  # current iteration that went through the algorithem
         self.prob_spec = problem_spec
-        self.file = open(r"C:\Users\freaz\Desktop\university\ai_Lab_1\pres.txt", "w+")
+        self.file = open(r"pres.txt", "w+")
+        self.cross_func = cross_types().select[crosstype]
 
     def init_population(self, pop_size, target_size):
 
@@ -73,47 +108,55 @@ class genetic_algorithem:
         self.population.sort()
 
     def elitism(self, esize):
-        for i in range(self.pop_size):
+        for i in range(esize):
             self.buffer[i] = self.population[i]
 
     def mutate(self, member):
         member.mutate(self.target_size, member)
 
-    def mate(self, cross_type):
+    def mate(self):
         esize = math.floor(self.pop_size * GA_ELITRATE)
         self.elitism(esize)
-        self.cross(cross_type)
+        self.cross()
 
-    def cross(self, cross_type):
+    def cross(self):
+
         for i in range(self.pop_size):
-            i1 = random.randint(0, GA_POPSIZE // 2)
-            i2 = random.randint(0, GA_POPSIZE // 2)
-            spos = random.randint(0, self.target_size)
-            # self.buffer[i] = Ga_struct()
             self.buffer[i] = self.prob_spec()
-            self.buffer[i].object = self.population[i1].object[0:spos] + self.population[i2].object[spos:]
+            citizen1= self.prob_spec()
+            citizen2= self.prob_spec()
+            i1 = random.randint(0, self.pop_size // 2)
+            i2 = random.randint(0, self.pop_size // 2)
+            # spos = random.randint(0, self.target_size)
+            # # self.buffer[i] = Ga_struct()
+            # self.buffer[i].object = self.population[i1].object[0:spos] + self.population[i2].object[spos:]
+            citizen1.object, citizen2.object = self.cross_func(self.population[i1].object, self.population[i2].object)
+            citizen1.calculate_fittness(self.target,self.target_size)
+            citizen2.calculate_fittness(self.target,self.target_size)
+            self.buffer[i]=citizen1 if citizen1.fitness<citizen2.fitness else citizen2
+
             if (secrets.randbelow(122) < GA_MUTATION):
                 self.mutate(self.buffer[i])
 
-
     def get_levels_fitness(self):
-        arr={}
+        arr = {}
         for i in self.population:
             if i.fitness in arr.keys():
-                arr[i.fitness]+=1
+                arr[i.fitness] += 1
             else:
-                arr[i.fitness]=1
+                arr[i.fitness] = 1
         for i in arr.keys():
-            self.file.write(str(i)+" "+str(arr[i])+"\n")
-
+            self.file.write(str(i) + " " + str(arr[i]) + "\n")
 
     def solve_genetic_problem(self):
         tick = time.time()
         sol_time = time.perf_counter()
         # random.seed(datetime.now())
         self.init_population(self.pop_size, self.target_size)
+        # for i in self.population:
+        #     print(i.object)
         for i in range(GA_MAXITER):
-            self.file.write("i" +str(self.iteration)+"\n")
+            self.file.write("i" + str(self.iteration) + "\n")
 
             self.iteration += 1
 
@@ -130,7 +173,7 @@ class genetic_algorithem:
             print_time((runtime, clockticks))
             if (self.population)[0].fitness == 0: break
 
-            self.mate(0)  # // mate the population together
+            self.mate()  # // mate the population together
 
             self.population, self.buffer = self.buffer, self.population  # // swap buffers
         self.file.close()
@@ -144,9 +187,11 @@ class genetic_algorithem:
 
 print_B = lambda x: print(f" Best: {x[0].object} ,fittness: {x[0].fitness} ", end=" ")
 
-#  that prints the best candidate
+#  prints mean and variance
 print_mean_var = lambda x: print(f"Mean: {x[0]} ,Variance: {x[1]}", end=" ")
+# prints time
 print_time = lambda x: print(f"Time :  {x[0]}  ticks: {x[1]}")
+# calculates variance
 variance = lambda x: math.sqrt((x[0] - x[1]) ** 2)
 
 
@@ -155,7 +200,7 @@ variance = lambda x: math.sqrt((x[0] - x[1]) ** 2)
 
 def main():
     overall_time = time.perf_counter()
-    GA = genetic_algorithem(GA_TARGET, TAR_size, GA_POPSIZE, DNA)
+    GA = genetic_algorithem(GA_TARGET, TAR_size, GA_POPSIZE, DNA, 2)
     GA.solve_genetic_problem()
     overall_time = time.perf_counter() - overall_time
 
