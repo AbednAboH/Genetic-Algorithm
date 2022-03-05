9fa4dffbc0639efdbe06f5734f68eb3fc59381bf
Abed <freazer122@gmail.com>
2022-03-05 13:13:36 +0200

    every algorithem now is also configurable and we can now use the functions as a lego !
    added :
    1. algorithem class , has all basic algorithem functions and parameters  , coupled with a class for each new problem that makes the rules in which the algorithem works on
    2. Genetic and pso now share the same base class , and we will then use the same DNA set of problem on both the algorithems !
    3. each added fittness function is automatically confiqured , if you add one just add it in the class and add a key for it in the selector (in the class)
    so far so good !

---
+++ PSO.py
@@ -0,0 +1,5 @@
+from algorithems import *
+
+class PSO(algortithem):
+    def __init__(self,target, tar_size, pop_size, problem_spec,fitnesstype):
+        algortithem.__init__(self,target, tar_size, pop_size, problem_spec,fitnesstype)
--- create_problem_sets.py
+++ create_problem_sets.py
@@ -1,22 +1,41 @@
 import random
+PENALTY=30
+HIGH_PENALTY=100
 
+class fitness_selector:
+    def __init__(self):
+        self.select={0:self.distance_fittness,1:self.bul_pqia}
 
+    def distance_fittness(self,object, target, target_size):
+        fitness=0
+        for j in range(target_size):
+            fit = ord(object[j]) - ord(target[j])
+            fitness += abs(fit)
+        return fitness
+
+    def bul_pqia(self,object, target, target_size):
+        fitness=0
+        for i in range(target_size):
+            if object[i]!=target[i]:
+                fitness += PENALTY if object[i] in target else HIGH_PENALTY
+        return fitness
+    ## fitness for pso
 # basic class for all problem sets because fittness and the member of the population are problem specific
 # and we have to eliminate problem specifc parameters from the Genetic algorithem
 # might add mutate !
-
 class parameters:
     def __init__(self):
+        self.fitnesstype=fitness_selector().select
         self.object = None
         self.fitness = 0
-
 
     # creates a member of the population
     def create_object(self,target_size):
         return self.object
 
     # function to calculate the fitness for this specific problem
-    def calculate_fittness(self,target, target_size):
+    def calculate_fittness(self,target, target_size,select_fitness):
+        self.fitness=self.fitnesstype[select_fitness](self.object,target,target_size)
         return self.fitness
 
     # for sorting purposes
@@ -35,15 +54,11 @@ class DNA(parameters):
             self.object += chr((random.randint(0, 90)) + 32)
         return self.object
 
-    def calculate_fittness(self,target, target_size):
-        for j in range(target_size):
-            fit = ord(self.object[j]) - ord(target[j])
-            self.fitness += abs(fit)
-        return self.fitness
     def mutate(self,target_size,member):
         ipos = random.randint(0,target_size - 1)
         delta = random.randint(0, 90) + 32
+        # can be written differently ,i.e more general
         member.object.replace(member.object[ipos], chr((ord(member.object[ipos]) + delta) % 122))
 
-
+# class for pso problem
 
